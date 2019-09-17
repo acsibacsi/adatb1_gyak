e7c04253d4e3edf298f9f699b7a7d587f1c89c9e
Fazekas Áron <a57k1a@INF.ELTE.HU>
2019-09-17 11:34:59 +0200

    Add elso gyak sql file

---
+++ elsogyak.sql
@@ -0,0 +1,36 @@
+--Masodik gyak
+--2
+select gyumolcs from szeret
+minus
+select gyumolcs from szeret where nev = 'Micimackó';
+
+--5
+select nev from szeret where gyumolcs = 'körte'
+union
+select nev from szeret where gyumolcs = 'alma';
+
+--6
+select nev from szeret where gyumolcs = 'körte'
+intersect
+select nev from szeret where gyumolcs = 'alma';
+
+-------------------------------------------------
+
+select * from dolgozo;
+
+--7
+select dnev, fizetes, fizetes*2, oazon from dolgozo where oazon = 10;
+
+--3
+select dnev, jutalek from dolgozo where jutalek > 600;
+
+--4
+select dnev, jutalek from dolgozo
+minus
+select dnev, jutalek from dolgozo where jutalek > 600;
+
+--9
+select dnev, fonoke from dolgozo where fonoke is null;
+
+--8
+select dnev, belepes from dolgozo where belepes > to_date('1982.01.01', 'YYYY.MM.DD');
